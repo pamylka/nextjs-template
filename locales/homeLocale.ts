7ab8b5c06e4b7b6974cd71540c25111f5f22d184
‪Ahmed Elbehairy‬‏ <ahmedelbehairy.72005@gmail.com>
2025-02-07 00:48:11 +0200

    added comingsoon locales

---
+++ locales/homeLocale.ts
@@ -0,0 +1,26 @@
+import { Locale } from './common';
+
+export type homeLocaleType = {
+  heading: string;
+  paragraph: string;
+  emailOrPhone: string;
+  addToWaitlist: string;
+};
+
+const homeLocale: Locale<homeLocaleType> = {
+  ar: {
+    heading: '🚀 !نَحنُ نَعمَلُ عَلَى شَيءٍ رائِعٍ، فَانتَظِرُونَا بِشَوقٍ',
+    paragraph: 'ضع بريدك الإلكتروني/رقم الهاتف حتى تكون أول من يعلم حين نطلقه!',
+    emailOrPhone: 'البريد الإلكتروني أو الهاتف',
+    addToWaitlist: 'انضم لقائمة الانتظار',
+  },
+  en: {
+    heading: 'Something great is coming soon!',
+    paragraph:
+      'Register your email/phone number to be the first to know when we launch!',
+    emailOrPhone: 'email or phone number',
+    addToWaitlist: 'Add to waitlist',
+  },
+};
+
+export default homeLocale;
