85b3f8ec2fe9520351adee6cb2cc5101fad59fe7
Yusuf - يُوسُفُ <zakiyousef15@gmail.com>
2025-02-15 22:37:30 +0200

    subscribe with email

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,34 +1,52 @@
-'use client';
+"use client";
 
-import { locales } from '@/locales/common';
-import homeLocale from '@/locales/homeLocale';
-import { useParams } from 'next/navigation';
-import { useState } from 'react';
-import Image from 'next/image';
-import logoBackground from '@/public/logo.svg';
-import logo from '@/public/main_logo.svg';
-import facebook from '@/public/facebook.svg';
-import youtube from '@/public/youtube.svg';
-import whatsapp from '@/public/whatsapp.svg';
-import email from '@/public/email.svg';
-import globe from '@/public/globe.svg';
-import Link from 'next/link';
+import { locales } from "@/locales/common";
+import homeLocale from "@/locales/homeLocale";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import Image from "next/image";
+import logoBackground from "@/public/logo.svg";
+import logo from "@/public/main_logo.svg";
+import facebook from "@/public/facebook.svg";
+import youtube from "@/public/youtube.svg";
+import whatsapp from "@/public/whatsapp.svg";
+import email from "@/public/email.svg";
+import globe from "@/public/globe.svg";
+import Link from "next/link";
 
 export default function Home() {
   const params = useParams();
   const localeKey = params.locale as locales;
-  const defaultLocale = homeLocale[localeKey] || homeLocale['en'];
-  const [locale] = useState(defaultLocale);
+  const locale = homeLocale[localeKey] || homeLocale["en"];
+  const [submitted, setSubmitted] = useState(false);
+
+  const formUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLSe6pjmiunSySDDDn6_aQzxsdI_m3O2259kisKyQNx7H9jHVEQ/formResponse";
+  const entryId = "entry.72372189";
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    fetch(formUrl, {
+      method: "POST",
+      body: formData,
+      mode: "no-cors",
+    }).then(() => setSubmitted(true));
+  };
+
 
   return (
     <>
       <div
-        className="relative min-h-screen flex flex-col items-center text-white text-center py-10 sm:py-12 md:py-16 bg-gradient-to-r from-dark-one to-dark-two"
+        className="relative min-h-screen flex flex-col items-center justify-between
+         text-white text-center py-10 sm:py-12 md:py-16
+         bg-gradient-to-r from-dark-one to-dark-two"
         style={{
           backgroundImage: `linear-gradient(to right, #333, rgb(51 51 51 / 0)), url(${logoBackground.src})`,
-          backgroundSize: 'contain',
-          backgroundPosition: 'right center',
-          backgroundRepeat: 'no-repeat',
+          backgroundSize: "contain",
+          backgroundPosition: "right center",
+          backgroundRepeat: "no-repeat",
         }}
       >
         {/* Logo */}
@@ -52,21 +70,28 @@ export default function Home() {
         </div>
 
         {/* Email Input & Button */}
-        <form className="mt-12 grid grid-cols-6 gap-2 md:gap-5 px-4 py-2 w-full xl:max-w-xl">
-          <input
-            type="email"
-            placeholder="email or phone number"
-            className="bg-white text-black rounded-xl col-span-4 md:text-lg px-4 py-2 md:py-4 outline-none placeholder-gray-400 focus:ring-2 focus:ring-yellow-500 placeholder:text-xs md:placeholder:text-lg"
-            aria-label="Email or phone number"
-          />
-          <button
-            type="submit"
-            className="bg-yellow-500 text-black font-thin text-xs md:text-base md:font-medium col-span-2 p-2 md:p-4
-            rounded-xl hover:bg-yellow-600 focus:ring-2 focus:ring-yellow-500"
-          >
-            {locale.addToWaitlist}
-          </button>
-        </form>
+        <div className="mt-12 w-full xl:max-w-xl px-4">
+          {!submitted ? (
+            <form onSubmit={handleSubmit} className="flex flex-col md:grid grid-cols-6 gap-2 md:gap-5 py-2">
+              <input
+                required
+                name={entryId}
+                type="email"
+                placeholder="email or phone number"
+                className="bg-white text-black rounded-xl col-span-4 md:text-lg px-4 py-2 md:py-4 outline-none placeholder-gray-400 focus:ring-2 focus:ring-yellow-500 placeholder:text-xs md:placeholder:text-lg"
+                aria-label="Email or phone number"
+              />
+              <button
+                type="submit"
+                className="bg-yellow-500 text-black font-thin text-xs md:text-base md:font-medium col-span-2 p-2 md:p-4 rounded-xl hover:bg-yellow-600 focus:ring-2 focus:ring-yellow-500"
+              >
+                {locale.addToWaitlist}
+              </button>
+            </form>
+          ) : (
+            <p className="text-green-500 text-lg">Thank you for subscribing!</p>
+          )}
+        </div>
 
         {/* Social Media Icons */}
         <div className="flex gap-4 mt-6 flex-wrap justify-center">
@@ -88,17 +113,8 @@ export default function Home() {
         </div>
 
         {/* Language Selector */}
-        <Link
-          href={locale.language}
-          className="flex items-center text-white gap-2 mt-4 "
-        >
-          <Image
-            src={globe}
-            alt="Language"
-            width={24}
-            height={24}
-            unoptimized
-          />
+        <Link href={locale.language} className="flex items-center text-white gap-2 mt-4 ">
+          <Image src={globe} alt="Language" width={24} height={24} unoptimized />
           <span className="text-sm sm:text-lg font-medium uppercase hover:underline hover:text-primary-color">
             {locale.language}
           </span>
